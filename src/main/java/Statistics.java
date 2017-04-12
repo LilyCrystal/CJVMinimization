2dadbf524840e8015cfd680299ba9502c21d59dc
Falk Hartmann <falk.hartmann@jugsaxony.org>
2017-04-12 08:00:38 +0200

    Added pom.xml to enable building with maven.

---
+++ src/main/java/Statistics.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+
+
+public class Statistics {
+	double[] data;
+    int size;   
+
+    public Statistics(double[] data) 
+    {
+        this.data = data;
+        size = data.length;
+    }   
+
+    double getMin()
+    {
+    	double minimum =  data[0];
+    	for(double a : data)
+    		if(a<=minimum)
+    			minimum = a;
+		return minimum;
+    		
+    }
+    
+    double getMax()
+    {
+    	double maximum =  data[0];
+    	for(double a : data)
+    		if(a>=maximum)
+    			maximum = a;
+		return maximum;
+    		
+    }
+    
+    double getMean()
+    {
+        double sum = 0.0;
+        for(double a : data)
+            sum += a;
+        return sum/size;
+    }
+
+    double getVariance()
+    {
+        double mean = getMean();
+        double temp = 0;
+        for(double a :data)
+            temp += (mean-a)*(mean-a);
+        return temp/size;
+    }
+
+    double getStdDev()
+    {
+        return Math.sqrt(getVariance());
+    }
+
+    public double median() 
+    {
+       Arrays.sort(data);
+
+       if (data.length % 2 == 0) 
+       {
+          return (data[(data.length / 2) - 1] + data[data.length / 2]) / 2.0;
+       } 
+       else 
+       {
+          return data[data.length / 2];
+       }
+    }
+
+}
